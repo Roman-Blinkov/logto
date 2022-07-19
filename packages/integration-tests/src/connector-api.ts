7b92608782ee9cc1c0a8ffae04f0fd79bc307f42
IceHe.xyz <icehe@silverhand.io>
2022-07-19 18:38:54 +0800

    test(test): connectors

---
+++ packages/integration-tests/src/connector-api.ts
@@ -0,0 +1,30 @@
+import { authedAdminApi } from '@/api';
+
+export const listConnectors = async () => authedAdminApi.get('connectors');
+
+export const getConnectorDetails = async (connectorId: string) =>
+  authedAdminApi.get(`connectors/${connectorId}`);
+
+export const updateConnectorConfig = async (connectorId: string, config: Record<string, unknown>) =>
+  authedAdminApi.patch({
+    url: `connectors/${connectorId}`,
+    json: { config },
+    headers: {
+      'development-user-id': 'integration-test-admin-user',
+    },
+  });
+
+export const enableConnector = async (connectorId: string) =>
+  authedAdminApi.patch({
+    url: `connectors/${connectorId}/enabled`,
+    json: { enabled: true },
+    headers: {
+      'development-user-id': 'integration-test-admin-user',
+    },
+  });
+
+export const deleteConnector = async (connectorId: string) =>
+  authedAdminApi.patch({
+    url: `connectors/${connectorId}/enabled`,
+    json: { enabled: false },
+  });
